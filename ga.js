9fefdff0edef9ad36aec6e43ca9a4d0290d92e9e
pcela <pcela@o2.pl>
2021-07-12 00:08:59 +0200

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,33 +1,18 @@
-(function(i,s,o,g,r,a,m){i[‘GoogleAnalyticsObject’]=r;i[r]=i[r]||function(){
-
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-
-})(window,document,’script’,’https://www.googletagmanager.com/gtag/js’,’ga‘);
-
-// Creates an adblock detection plugin.
-
-ga(‘provide’, ‘adblockTracker’, function(tracker, opts) {
-
-var ad = document.createElement(‘ins’);
-
-ad.className = ‘AdSense’; //Name of ad partner you’re working with.
-
-ad.style.display = ‘block’;
-
-ad.style.position = ‘absolute’;
-
-ad.style.top = ‘-1px’;
-
-ad.style.height = ‘1px’;
-
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+// Wtyczka do śledzenia AdBlock
+ga('provide', 'adblockTracker', function(tracker, opts) {
+var ad = document.createElement('ins');
+ad.className = 'AdSense';
+ad.style.display = 'block';
+ad.style.position = 'absolute';
+ad.style.top = '-1px';
+ad.style.height = '1px';
 document.body.appendChild(ad);
-
-tracker.set(‘dimension’ + opts.dimensionIndex, !ad.clientHeight);
-
+tracker.set('dimension' + opts.dimensionIndex, !ad.clientHeight);
 document.body.removeChild(ad);
-
 });
 
 ga(‘create’, ‘UA-78842172-4’, ‘auto’); //Replace UA-XXXXX-Y with your tracking ID.
